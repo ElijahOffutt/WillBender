d42e7be8477b0642b680383b8e3929818df36970
ElijahOffutt <Einzenheim@yahoo.com>
2023-08-28 14:45:44 -0400

    updates to start changing the front splash view

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,23 @@
-<script setup>
+<script setup lang="ts">
 
 /* template specific */
 
 import Counter from './modules/counter/mod.vue';
 import Fetcher from './modules/fetcher/mod.vue';
+import Splash from './modules/Splash/index.vue';
+import Left from './modules/Navigation/Left/index.vue';
+import Right from './modules/Navigation/Right/index.vue'
+
+let logged = true;
 
 </script>
 
 <template>
   <v-app>
+    <Splash v-if="!logged" />
+    <Left />
     <v-main class="bg-grey-lighten-4">
       <v-container class="mx-auto" style="max-width: 768px;">
-
-        <counter />
-
-        <fetcher class="mt-3" />
 
       </v-container>
     </v-main>
---
+++ src/modules/Navigation/Left/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-navigation-drawer right disable-resize-watcher expand-on-hover elevation="6" app show>
+        <h2 class="ml-4 mt-4">Prompts</h2>
+        <v-list>
+            <v-list-item>
+                <v-btn color="success" density="compact" block>prompt name</v-btn>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
