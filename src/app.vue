1d16eee3df8fdf3c3bdbb54acdd0f65c74513675
ElijahOffutt <Einzenheim@yahoo.com>
2023-09-05 19:32:13 -0400

    updates to the first page to get the chatgpt api key before adding the google bard one

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
+import { ref } from 'vue';
 /* template specific */
-import Splash from './modules/Splash/index.vue';
+
+let tab = ref<String>("one");
+let ChatGPT_API_Key = ref<String>("")
 
 let logged = true;
 
@@ -9,19 +11,34 @@ let logged = true;
 
 <template>
   <v-app>
-    <v-main class="bg-grey-lighten-4">
+    <v-main>
       <v-container class="mx-auto" style="max-width: 768px;">
-          <h1>Will Bender</h1>
-          <b><i>a ai tool for powerful prompting</i></b>
-          <v-tabs>
-            <v-tab>chat gpt</v-tab>
-            <v-tab>google bard</v-tab>
-            <v-window>
-              <v-window-item>
-                <v-input></v-input>
-              </v-window-item>
-            </v-window>
-          </v-tabs>
+
+        <h1 class="text-h3 font-weight-medium">Will Bender</h1>
+        <p style="text-indent: 5px;">An ai tool for powerful prompting</p>
+
+        <v-tabs fixed-tabs v-model="tab">
+          <v-tab value="one">chat gpt</v-tab>
+          <v-tab value="two">google bard</v-tab>
+        </v-tabs>
+
+        <v-window v-model="tab">
+          <v-window-item value="one">
+            <h3 class="mt-3">Please Input Credentials</h3>
+            <v-row>
+              <v-col class="grow">
+                <v-text-field solo label="Chat GPT API Key" v-model="ChatGPT_API_Key" />
+              </v-col>
+              <v-col class="shrink">
+                <v-btn color="success" block x-large>configure</v-btn>
+              </v-col>
+            </v-row>
+          </v-window-item>
+          <v-window-item value="two">
+            <h3>Please Input Credentials</h3>
+          </v-window-item>
+        </v-window>
+
       </v-container>
     </v-main>
   </v-app>
