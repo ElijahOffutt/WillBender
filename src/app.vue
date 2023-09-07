b47eeae3929f230ad6b580a97d358a86e3e82149
ElijahOffutt <Einzenheim@yahoo.com>
2023-09-07 14:01:06 -0400

    updates for thursday and creating the new task workflow

--- src/app.vue
+++ src/app.vue
@@ -1,43 +1,51 @@
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue';
-/* template specific */
+import Create from './modules/Create/index.vue'
 
-let tab = ref<String>("one");
-let ChatGPT_API_Key = ref<String>("")
-
-let logged = true;
+let tasks = ref([])
+let creating = ref(false)
 
 </script>
 
 <template>
   <v-app>
     <v-main>
-      <v-container class="mx-auto" style="max-width: 768px;">
+      <v-container fluid class="mx-auto" style="max-width: 768px;">
 
-        <h1 class="text-h3 font-weight-medium">Will Bender</h1>
-        <p style="text-indent: 5px;">An ai tool for powerful prompting</p>
+        <v-row justify="space-between">
+          <v-col cols="10">
+            <h1 class="text-h3 font-weight-medium">Will Bender</h1>
+          </v-col>
+          <v-col cols="2">
+            <v-btn @click="creating = !creating" :color="creating ? 'warning' : 'success'"
+              :icon="`mdi-${creating ? 'close' : 'plus'}`" />
+          </v-col>
+        </v-row>
+        <p class="my-3" style="text-indent: 5px;">An ai tool for powerful prompting {{ creating }}</p>
+        <v-divider class="mb-5" />
 
-        <v-tabs fixed-tabs v-model="tab">
-          <v-tab value="one">chat gpt</v-tab>
-          <v-tab value="two">google bard</v-tab>
-        </v-tabs>
+        <!-- <v-row>
+          <v-col cols="8">
+            <v-text-field density="compact" label="ChatGPT API Key" v-model="ChatGPT_API_Key" solo />
+          </v-col>
+          <v-col cols="4">
+            <v-btn @click="fire" size="large" prepend-icon="mdi-check" block :disabled="ChatGPT_API_Key.length == 0"
+              color="success">configure</v-btn>
+          </v-col>
+          <v-col cols="12">
+            <p>{{ text }}</p>
+          </v-col>
+        </v-row> -->
 
-        <v-window v-model="tab">
-          <v-window-item value="one">
-            <h3 class="mt-3">Please Input Credentials</h3>
-            <v-row>
-              <v-col class="grow">
-                <v-text-field solo label="Chat GPT API Key" v-model="ChatGPT_API_Key" />
-              </v-col>
-              <v-col class="shrink">
-                <v-btn color="success" block x-large>configure</v-btn>
-              </v-col>
-            </v-row>
-          </v-window-item>
-          <v-window-item value="two">
-            <h3>Please Input Credentials</h3>
-          </v-window-item>
-        </v-window>
+        <v-row v-if="!creating">
+          <v-col v-for="task in tasks" cols="12">
+            <v-btn block>
+              <span style="text-transform: none;">task title</span>
+            </v-btn>
+          </v-col>
+        </v-row>
+
+        <Create v-if="creating" />
 
       </v-container>
     </v-main>
---
+++ src/modules/Create/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card elevation="3">
+        <v-card-title>
+            <v-text-field variant="solo" density="compact" @keyup.enter="titleEditMode = false" v-if="titleEditMode"
+                v-model="title" label="Task Title" solo />
+            <h3 @click="titleEditMode = true" v-if="!titleEditMode && title.length > 0">
+                <b>{{ title }}</b>
+                <v-tooltip text="Edit Title" location="top" activator="parent" />
+            </h3>
+        </v-card-title>
+        <v-card-text>
+            <v-textarea v-model="prompt.task" variant="solo" label="Task Text" />
+        </v-card-text>
+        <p>{{ prompt.task.value }}</p>
+        <v-divider />
+        <v-card-actions>
+            <v-btn variant="default" color="blue">Persona</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup>
+// goal/what you are tryingto accomplish
+// format
+// main six are task, context, exemplars, persona, format and tone
+import { ref, reactive } from 'vue'
+
+let titleEditMode = ref(true)
+let title = ref('')
+let prompt = reactive({
+    task: ''
+})
+
+let toggle = () => { titleEditMode.value = !titleEditMode.value }
+
+</script>
+
+<style lang="scss"></style>
