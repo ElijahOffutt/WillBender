ba0f0c736b7499a44e8ac816253671f371d9382a
ElijahOffutt <Einzenheim@yahoo.com>
2023-09-07 14:07:20 -0400

    updated the new features

--- src/app.vue
+++ src/app.vue
@@ -2,23 +2,14 @@
 import { ref } from 'vue';
 import Create from './modules/Create/index.vue'
 
-<<<<<<< HEAD
 let tasks = ref([])
 let creating = ref(false)
-=======
-let tab = ref<String>("one");
-let ChatGPT_API_Key = ref<String>("")
-
-function myFunc() { alert("het there") }
-let logged = true;
->>>>>>> 32ee06208849591a40d08a0de08fecfda418fbf4
 
 </script>
 
 <template>
   <v-app>
     <v-main>
-<<<<<<< HEAD
       <v-container fluid class="mx-auto" style="max-width: 768px;">
 
         <v-row justify="space-between">
@@ -55,34 +46,6 @@ let logged = true;
         </v-row>
 
         <Create v-if="creating" />
-=======
-      <v-container class="mx-auto" style="max-width: 768px;">
-
-        <h1 class="text-h3 font-weight-medium">Will Bender</h1>
-        <p style="text-indent: 5px;">An ai tool for powerful prompting</p>
-
-        <v-tabs fixed-tabs v-model="tab">
-          <v-tab value="one">chat gpt</v-tab>
-          <v-tab value="two">google bard</v-tab>
-        </v-tabs>
-
-        <v-window v-model="tab">
-          <v-window-item value="one">
-            <h3 class="mt-3">Please Input Credentials</h3>
-            <v-row>
-              <v-col class="grow">
-                <v-text-field solo label="Chat GPT API Key" v-model="ChatGPT_API_Key" />
-              </v-col>
-              <v-col class="shrink">
-                <v-btn @click="myFunc" color="success" block x-large>configure</v-btn>
-              </v-col>
-            </v-row>
-          </v-window-item>
-          <v-window-item value="two">
-            <h3>Please Input Credentials</h3>
-          </v-window-item>
-        </v-window>
->>>>>>> 32ee06208849591a40d08a0de08fecfda418fbf4
 
       </v-container>
     </v-main>
