0e49a89a38195815b00a21434bcaf305ac2e0b3a
ElijahOffutt <Einzenheim@yahoo.com>
2023-09-11 20:56:50 -0400

    updates to the form to show colors for each feild

--- src/modules/Create/index.vue
+++ src/modules/Create/index.vue
@@ -9,16 +9,30 @@
             </h3>
         </v-card-title>
         <v-card-text>
-            <v-textarea v-model="prompt.task" variant="solo" label="Task Text" />
-            <v-btn color="green" block>save task</v-btn>
+            <v-card v-for="(value, key) in prompt" :style="`border: solid 3px ${color(key)};`" class="mb-3">
+                <v-card-text class="pb-0 mb-0">
+                    <v-textarea v-model="prompt[key]" variant="outlined" :label="key" />
+                </v-card-text>
+                <v-card-actions class="mt-0 pt-0">
+                    <v-row>
+                        <v-col :cols="key == 'task' ? 12 : 6">
+                            <v-btn :disable="prompt[key].length > 5" size="small" color="success" variant="outlined" block
+                                append-icon="mdi-check">save</v-btn>
+                        </v-col>
+                        <v-col v-if="key != 'task'" cols="6">
+                            <v-btn size="small" @click="delete prompt[key]" color="error" variant="outlined" block
+                                append-icon="mdi-delete">delete</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-card-actions>
+            </v-card>
         </v-card-text>
-        <v-btn v-for="(item, name) in prompt">{{name}}</v-btn>
-        <p>Payload: {{ prompt }}</p>
         <v-divider />
         <v-card-actions>
-            <v-btn v-for="{name, color} in options" variant="tonal" 
-            @click="prompt[name] = ''"
-            :color="color">{{name}}</v-btn>
+            <template v-for="{ name, color } in options">
+                <v-btn v-if="name != 'task'" variant="tonal" @click="prompt[name] = ''" :color="color">{{ name
+                }}</v-btn>
+            </template>
         </v-card-actions>
     </v-card>
 </template>
@@ -30,18 +44,26 @@
 import { ref, reactive, computed } from 'vue'
 
 let titleEditMode = ref(true)
-const options = [
-    { name: 'persona', color: 'blue'},
-    { name: 'tone', color: 'purple'},
-    { name: 'rules', color: 'yellow'},
-    { name: 'includes', color: 'green'},
-    { name: 'negatives', color: 'red'},
-    { name: 'examples', color: 'orange'},
-    { name: 'format', color: 'grey'},
-]
+let title = ref('')
+const options = computed(() => {
+    const list = [
+        { name: 'task', color: 'black' },
+        { name: 'persona', color: 'blue' },
+        { name: 'tone', color: 'purple' },
+        { name: 'rules', color: 'yellow' },
+        { name: 'includes', color: 'green' },
+        { name: 'negatives', color: 'red' },
+        { name: 'examples', color: 'orange' },
+        { name: 'format', color: 'grey' },
+    ]
+    let filtered = list.filter(item => item.name != prompt[item.name]);
+    return filtered
+})
 let prompt = reactive({
     task: '',
 })
+
+let color = (key) => options.value.find(item => item.name == key).color
 
 let toggle = () => { titleEditMode.value = !titleEditMode.value }
 
